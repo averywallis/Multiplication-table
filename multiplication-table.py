13c47dfc22e8ac5dfa6ddb383ea294a6b58fc3c2
averywallis <averybwallis@gmail.com>
2016-02-03 13:53:43 -0500

    Updated from Brython Server: 2/3/2016 1:53:42 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,7 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Avery Wallis
+Credit: None so Far
 Assignment:
 
 Write and submit a Python program that prints a multiplication table. The user 
@@ -21,3 +21,5 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+Width of multiplication table: 10
+Height of multiplication table: 8
